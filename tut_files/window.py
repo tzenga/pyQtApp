4605d5663ce94213e16ce9883741f36727455e2c
Andrew Tzeng <tzeng.a@gmail.com>
2019-02-20 14:13:32 -0500

    organized images to image folder, added toolbar from tutorial

--- tut_files/window.py
+++ tut_files/window.py
@@ -7,7 +7,7 @@ class Window(QtGui.QMainWindow):
         super(Window, self).__init__()
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("PyQT tuts!")
-        self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+        self.setWindowIcon(QtGui.QIcon('../images/logo.png'))
 
         extractAction = QtGui.QAction("&GET TO THE CHOPPAH!!!", self)
         extractAction.setShortcut("Ctrl+Q")
@@ -27,6 +27,13 @@ class Window(QtGui.QMainWindow):
         btn.clicked.connect(self.close_application)
         btn.resize(btn.minimumSizeHint())
         btn.move(0,100)
+
+        extractAction = QtGui.QAction(QtGui.QIcon('../images/todachoppa.png'), 'logo', self)
+        extractAction.triggered.connect(self.close_application)
+
+        self.toolBar = self.addToolBar("Extraction")
+        self.toolBar.addAction(extractAction)
+
         self.show()
 
     def close_application(self):
